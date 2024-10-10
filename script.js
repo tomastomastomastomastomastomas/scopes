0d260f9183beac508038658b603f494ee8a810b5
tomas <tomas.gentile@gm2dev.com>
2024-10-10 10:05:05 -0300

    feat(script): add new function to modify global variable

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ function myLittleFunction() {
     let blockVar = "bloque";
   }
 }
+
+let closureGlobalVar = "global";
+
+function otherFunction(paramVar) {
+  let functionVar = "funcion";
+  if (paramVar) {
+    let blockVar = "bloque";
+  }
+  function extraFunction() {
+    closureGlobalVar += " variable";
+  }
+  extraFunction();
+}
